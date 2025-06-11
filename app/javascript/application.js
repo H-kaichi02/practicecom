55e2f5bd867f10fd0c270733e756b1d43e762b81
H-kaichi02 <kaichi.hirate.1381@gmail.com>
2025-06-11 10:59:43 +0900

    試行錯誤
    jsファイルの記述について試行錯誤してた。操作設定はコマンドで変わりはないから何とか一つにまとめられないか？

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,4 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "main"
+import "shinkuhadouken"
---
+++ app/javascript/shinkuhadouken.js
@@ -0,0 +1,4 @@
+let inputHistory = [];
+const maxFrames = 20;
+const command = ["↓", "↘", "→", "↓", "↘", "→", "P"];
+const maxDelay = 100; // 各ステップ間の最大許容時間（ms）
